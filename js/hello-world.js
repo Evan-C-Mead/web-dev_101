54b1ab054df93d875b3cbe568416150d20c62b34
Evan-C-Mead <evan.c.mead@gmail.com>
2023-01-08 00:35:53 -0600

    init commit

---
+++ js/hello-world.js
@@ -0,0 +1,17 @@
+"use strict";
+// this is a comment
+// use them to leave notes, documentation, tips, or to section off a block of code for whatever you see fit
+
+(function() {
+    console.log("Hello world!");
+    console.log("This is neat");
+
+    let myString = "This is my string"
+    console.log(myString);
+
+    let myNumber = 4;
+    console.log(myNumber);
+
+    let myStringNumber = "My number is: "
+    console.log(myStringNumber + myNumber);
+})();
