70916762c04ab89eac4aaaeb6645c9823c150cf4
Evan-C-Mead <evan.c.mead@gmail.com>
2023-04-29 22:29:55 -0500

    grammar isn't my strong point

--- js/say-it.js
+++ js/say-it.js
@@ -1,13 +1,15 @@
 "use strict";
 
+(function () {
 function sayIt() {
     let count = 0;
     let x = 1;
     let randCount = Math.floor(Math.random() * 10 + 1)
     for (x; x <= randCount; x++) {
         count++;
-        console.log(`this is scentence #${count}`);
+        console.log(`this is sentence #${count}`);
     }
 }
 
 sayIt();
+})();
