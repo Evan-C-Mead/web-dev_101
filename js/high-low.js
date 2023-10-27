e29bb6b68c69d245eeb9f72b435e641f5cc70cf3
Evan-C-Mead <evan.c.mead@gmail.com>
2023-10-27 13:57:28 -0500

    guess function works - styling needed on sub form

--- js/high-low.js
+++ js/high-low.js
@@ -2,9 +2,28 @@
 
 function guess() {
     let randNum = Math.floor(Math.random() * 100 + 1);
-    let guessCount = 0;
+    let guessCount = 1;
 
     console.log(randNum);
+
+    document.getElementById("submitguess").onclick = function () {
+        let currentGuess = document.getElementById("guessField").value;
+
+        if (currentGuess == randNum && guessCount == 1) {
+            alert("CONGRATULATIONS!!! IT TOOK YOU " + guessCount + " TRY");
+        }
+        else if (currentGuess == randNum) {
+            alert("CONGRATULATIONS!!! IT TOOK YOU " + guessCount + " TRIES")
+        }
+        else if (currentGuess > randNum) {
+            guessCount++;
+            alert("GUESS A SMALLER NUMBER THAN " + currentGuess);
+        }
+        else {
+            guessCount++;
+            alert("TRY A GREATER NUMBER THAN " + currentGuess);
+        }
+    }
 }
 
 guess();
