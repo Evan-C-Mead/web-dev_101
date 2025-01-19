cc7d2a6cb94e01f4d87d2295bd2233d0651f2ad9
Evan-C-Mead <evan.c.mead@gmail.com>
2025-01-18 22:55:22 -0600

    hello changes

--- js/hello.js
+++ js/hello.js
@@ -11,7 +11,7 @@ function sayGreeting() {
   } else if (hrs < 7) {
     greeting = `Coffee time.`;
   } else if (hrs < 11) {
-    greeting = `Hello. Good morning.`;
+    greeting = `Good morning.`;
   } else if (hrs < 13) {
     greeting = `${broham}, it's time for lunch.`;
   } else if (hrs < 17) {
@@ -19,7 +19,7 @@ function sayGreeting() {
   } else if (hrs < 19) {
     greeting = `Beef. It's what's for dinner.`;
   } else if (hrs < 22) {
-    greeting = `Hello. Good evening.`;
+    greeting = `Good evening.`;
   } else {
     greeting = `${broham}, go to bed.`;
   }
